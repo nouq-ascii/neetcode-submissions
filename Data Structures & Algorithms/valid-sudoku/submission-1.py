018fd145baace4abb96a0b9426696bfe15f63f23
Nour <abejnor185@gmail.com>
2026-09-16 09:34:00 +0200

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        n = 9
+        countcol = [set() for _ in range(n)]
+        countbox = [set() for _ in range(n)]
+        countrow = [set() for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                num = board[i][j]
+                if num != ".":
+                    box_index = (3*(math.ceil((i + 1) / 3)-1)) + (math.ceil((j + 1) / 3)-1)
+                    if num in countrow[i]:
+                        return False
+                    else: 
+                        countrow[i].add(num)
+                    if num in countcol[j]:
+                        return False
+                    else: 
+                        countcol[j].add(num)
+                    if num in countbox[box_index]:
+                        return False
+                    else:
+                        countbox[box_index].add(num)
+        return True
+
+
+
+            
+        
